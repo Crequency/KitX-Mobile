d8ff91984bafebe64fac4678db458971bba06b9c
Dynesshely <catrol@qq.com>
2022-12-30 06:56:12 +0800

    💾 Feat: Try MAUI with dotnet 7

---
+++ KitX Mobile MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KitX_Mobile_MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ KitX Mobile MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace KitX_Mobile_MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
