dcec0d71a2077d01ff8ffae64be1b81f88504d4b
Dynesshely <catrol@qq.com>
2023-01-05 11:23:00 +0800

    💾 Feat: 加速度传感器数据呈现; 字号大小调整

--- kitx_mobile/lib/pages/test_pages/device_sensors.dart
+++ kitx_mobile/lib/pages/test_pages/device_sensors.dart
@@ -13,37 +13,47 @@ class DeviceSensorsPage extends StatefulWidget {
 }
 
 class _DeviceSensorsPage extends State<DeviceSensorsPage> {
-  double x = 0, y = 0, z = 0;
+  double dir_x = 0, dir_y = 0, dir_z = 0;
+  double acc_x = 0, acc_y = 0, acc_z = 0;
   String direction_x = "none";
   String direction_y = "none";
   String direction_z = "none";
 
   @override
   void initState() {
-    gyroscopeEvents.listen((GyroscopeEvent event) {
+    gyroscopeEvents.listen((event) {
       print(event);
 
-      x = event.x;
-      y = event.y;
-      z = event.z;
+      dir_x = event.x;
+      dir_y = event.y;
+      dir_z = event.z;
 
       //rough calculation, you can use
       //advance formula to calculate the orentation
-      if (x >= 0)
+      if (dir_x >= 0)
         direction_x = "back";
       else
         direction_x = "forward";
-      if (y >= 0)
+      if (dir_y >= 0)
         direction_y = "right";
       else
         direction_y = "left";
-      if (z >= 0)
+      if (dir_z >= 0)
         direction_z = "👈";
       else
         direction_z = "👉";
 
       setState(() {});
     });
+
+    userAccelerometerEvents.listen((event) {
+      print(event);
+
+      acc_x = event.x;
+      acc_y = event.y;
+      acc_z = event.z;
+    });
+
     super.initState();
   }
 
@@ -72,34 +82,24 @@ class _DeviceSensorsPage extends State<DeviceSensorsPage> {
           alignment: Alignment.center,
           padding: EdgeInsets.all(30),
           child: ListView(children: [
-            Text("Gyroscope Data", style: TextStyle(fontSize: 42)),
+            Text("Gyroscope Data", style: TextStyle(fontSize: 32)),
             Row(
                 mainAxisSize: MainAxisSize.max,
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text(
-                    direction_x,
-                    style: TextStyle(fontSize: 30),
-                  ),
-                  Text(
-                    direction_y,
-                    style: TextStyle(fontSize: 30),
-                  ),
-                  Text(
-                    direction_z,
-                    style: TextStyle(fontSize: 30),
-                  ),
+                  Text(direction_x, style: TextStyle(fontSize: 26),),
+                  Text(direction_y, style: TextStyle(fontSize: 26),),
+                  Text(direction_z, style: TextStyle(fontSize: 26),),
                 ]),
-            Text("x: $x", style: TextStyle(fontSize: 14)),
-            Text("y: $y", style: TextStyle(fontSize: 14)),
-            Text("z: $z", style: TextStyle(fontSize: 14)),
-            // Cube(
-            //   onSceneCreated: (Scene scene) {
-            //     scene.world.add(Object(fileName: 'assets/cube/KitX-Icon-1.obj'));
-            //   },
-            // ),
-            Text("Vibration Test", style: TextStyle(fontSize: 42)),
+            Text("x: $dir_x", style: TextStyle(fontSize: 14)),
+            Text("y: $dir_y", style: TextStyle(fontSize: 14)),
+            Text("z: $dir_z", style: TextStyle(fontSize: 14)),
+            Text("Acceleration Data", style: TextStyle(fontSize: 32)),
+            Text("x: $acc_x", style: TextStyle(fontSize: 14)),
+            Text("y: $acc_y", style: TextStyle(fontSize: 14)),
+            Text("z: $acc_z", style: TextStyle(fontSize: 14)),
+            Text("Vibration Test", style: TextStyle(fontSize: 32)),
             TextField(
               decoration:
                   new InputDecoration(labelText: "Vibration Duration (ms)"),
