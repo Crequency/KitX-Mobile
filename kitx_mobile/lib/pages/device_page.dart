e39c3b73cbf3d2231a1cb71cd6ce80170ef5d1ff
Dynesshely <catrol@qq.com>
2023-01-30 04:41:19 +0800

    💾 Feat: 设备列表页更好的交互动效

--- kitx_mobile/lib/pages/device_page.dart
+++ kitx_mobile/lib/pages/device_page.dart
@@ -28,10 +28,7 @@ class _DevicePage extends State<DevicePage> {
               () => ListView.builder(
                 itemCount: global.devices.length,
                 itemBuilder: (_, int index) {
-                  return Container(
-                    // margin: EdgeInsets.only(left: 0, top: 10, right: 0, bottom: 0),
-                    child: createDeviceCard(global.devices.deviceInfoList[index], index),
-                  );
+                  return createDeviceCard(context, global.devices.deviceInfoList[index], index);
                 },
               )
             ),
@@ -39,7 +36,7 @@ class _DevicePage extends State<DevicePage> {
   }
 }
 
-Widget createDeviceCard(DeviceInfoStruct info, int index) {
+Widget createDeviceCard(BuildContext context, DeviceInfoStruct info, int index) {
   IconData _iconStyle = Convert(info.DeviceOSType);
   Icon _icon = Icon(
     _iconStyle,
@@ -48,80 +45,85 @@ Widget createDeviceCard(DeviceInfoStruct info, int index) {
   return FractionallySizedBox(
     widthFactor: 0.9,
     child: Padding(
-        padding: EdgeInsets.fromLTRB(10, index == 0 ? 40 : 10, 10, 10),
+        padding: EdgeInsets.fromLTRB(0, index == 0 ? 25 : 0, 0, 20),
         child: Card(
-          child: Container(
-            margin: EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.fromLTRB(5, 2, 0, 2),
-                      child: _icon,
-                    ),
-                    Expanded(
-                      child: Container(
-                        margin: EdgeInsets.fromLTRB(10, 3, 0, 3),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            SingleChildScrollView(
-                              scrollDirection: Axis.horizontal,
-                              child: Text(info.DeviceName == global.DeviceName
-                                  ? info.DeviceName + " " + "DevicePage_LocalDevice".tr
-                                  : info.DeviceName),
-                            ),
-                            SingleChildScrollView(
-                              scrollDirection: Axis.horizontal,
-                              child: Text(info.DeviceOSVersion),
-                            ),
-                          ],
-                        ),
+          clipBehavior: Clip.hardEdge,
+          child: InkWell(
+            splashColor: context.iconColor?.withOpacity(0.3),
+            onTap: () => print(info.DeviceName + " card tapped."),
+            child: Container(
+              margin: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.fromLTRB(5, 2, 0, 2),
+                        child: _icon,
                       ),
-                    ),
-                  ],
-                ),
-                Text(
-                  datetimeToShortString(info.SendTime),
-                  style: TextStyle(
-                    fontSize: 14,
-                  ),
-                ),
-                Text(
-                  "DevicePage_PluginsCountText".tr +
-                      info.PluginsCount.toString(),
-                  style: TextStyle(
-                    fontSize: 14,
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      info.IPv4,
-                      style: TextStyle(
-                        fontSize: 10,
-                      ),
-                    ),
-                    Flexible(
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Text(
-                          info.IPv6,
-                          textAlign: TextAlign.right,
-                          style: TextStyle(
-                            fontSize: 10,
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(10, 3, 0, 3),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              SingleChildScrollView(
+                                scrollDirection: Axis.horizontal,
+                                child: Text(info.DeviceName == global.DeviceName
+                                    ? info.DeviceName + " " + "DevicePage_LocalDevice".tr
+                                    : info.DeviceName),
+                              ),
+                              SingleChildScrollView(
+                                scrollDirection: Axis.horizontal,
+                                child: Text(info.DeviceOSVersion),
+                              ),
+                            ],
                           ),
                         ),
                       ),
+                    ],
+                  ),
+                  Text(
+                    datetimeToShortString(info.SendTime),
+                    style: TextStyle(
+                      fontSize: 14,
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  Text(
+                    "DevicePage_PluginsCountText".tr +
+                        info.PluginsCount.toString(),
+                    style: TextStyle(
+                      fontSize: 14,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        info.IPv4,
+                        style: TextStyle(
+                          fontSize: 10,
+                        ),
+                      ),
+                      Flexible(
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Text(
+                            info.IPv6,
+                            textAlign: TextAlign.right,
+                            style: TextStyle(
+                              fontSize: 10,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
+          )
         )
         ),
   );
