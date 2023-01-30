92f72c15ace9e46673309184030d6d2016549dc6
Dynesshely <catrol@qq.com>
2023-01-30 16:05:30 +0800

    💾 Feat: 加速度传感器数据模块拆分

--- kitx_mobile/lib/pages/test_pages/device_sensors.dart
+++ kitx_mobile/lib/pages/test_pages/device_sensors.dart
@@ -1,5 +1,6 @@
 ﻿import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:kitx_mobile/pages/test_pages/sensors_display_stands/gyroscope_display_stand.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 
 import 'sensors_display_stands/vibration_display_stand.dart';
@@ -10,39 +11,10 @@ class DeviceSensorsPage extends StatefulWidget {
 }
 
 class _DeviceSensorsPage extends State<DeviceSensorsPage> {
-  final dir_x = 0.0.obs, dir_y = 0.0.obs, dir_z = 0.0.obs;
   final acc_x = 0.0.obs, acc_y = 0.0.obs, acc_z = 0.0.obs;
-  final direction_x = "none".obs;
-  final direction_y = "none".obs;
-  final direction_z = "none".obs;
 
   @override
   void initState() {
-    gyroscopeEvents.listen((event) {
-      print(event);
-
-      dir_x.value = event.x;
-      dir_y.value = event.y;
-      dir_z.value = event.z;
-
-      //rough calculation, you can use
-      //advance formula to calculate the orientation
-      if (dir_x >= 0)
-        direction_x.value = "back";
-      else
-        direction_x.value = "forward";
-
-      if (dir_y >= 0)
-        direction_y.value = "right";
-      else
-        direction_y.value = "left";
-
-      if (dir_z >= 0)
-        direction_z.value = "👈";
-      else
-        direction_z.value = "👉";
-    });
-
     userAccelerometerEvents.listen((event) {
       print(event);
 
@@ -61,34 +33,7 @@ class _DeviceSensorsPage extends State<DeviceSensorsPage> {
           alignment: Alignment.center,
           padding: EdgeInsets.all(30),
           child: ListView(children: [
-            Text("Gyroscope Data", style: TextStyle(fontSize: 32)),
-            Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Obx(() => Text(
-                        direction_x.value,
-                        style: TextStyle(fontSize: 26),
-                      )),
-                  Obx(() => Text(
-                        direction_y.value,
-                        style: TextStyle(fontSize: 26),
-                      )),
-                  Obx(() => Text(
-                        direction_z.value,
-                        style: TextStyle(fontSize: 26),
-                      )),
-                ]),
-            Obx(
-              () => Text("x: ${dir_x.value}", style: TextStyle(fontSize: 14)),
-            ),
-            Obx(
-              () => Text("y: ${dir_y.value}", style: TextStyle(fontSize: 14)),
-            ),
-            Obx(
-              () => Text("z: ${dir_z.value}", style: TextStyle(fontSize: 14)),
-            ),
+            GyroscopeDisplayStand(),
             Text("Acceleration Data", style: TextStyle(fontSize: 32)),
             Obx(
               () => Text("x: ${acc_x.value}", style: TextStyle(fontSize: 14)),
---
+++ kitx_mobile/lib/pages/test_pages/sensors_display_stands/gyroscope_display_stand.dart
@@ -0,0 +1,80 @@
+﻿import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+
+class GyroscopeDisplayStand extends StatefulWidget{
+  @override
+  GyroscopeDisplayStandState createState() => GyroscopeDisplayStandState();
+}
+
+class GyroscopeDisplayStandState extends State<GyroscopeDisplayStand>{
+  final dir_x = 0.0.obs, dir_y = 0.0.obs, dir_z = 0.0.obs;
+  final direction_x = "none".obs, direction_y = "none".obs, direction_z = "none".obs;
+
+  @override
+  void initState(){
+    gyroscopeEvents.listen((event) {
+      print(event);
+
+      dir_x.value = event.x;
+      dir_y.value = event.y;
+      dir_z.value = event.z;
+
+      //rough calculation, you can use
+      //advance formula to calculate the orientation
+      if (dir_x >= 0)
+        direction_x.value = "back";
+      else
+        direction_x.value = "forward";
+
+      if (dir_y >= 0)
+        direction_y.value = "right";
+      else
+        direction_y.value = "left";
+
+      if (dir_z >= 0)
+        direction_z.value = "👈";
+      else
+        direction_z.value = "👉";
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Gyroscope Data", style: TextStyle(fontSize: 32)),
+        Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Obx(() => Text(
+                direction_x.value,
+                style: TextStyle(fontSize: 26),
+              )),
+              Obx(() => Text(
+                direction_y.value,
+                style: TextStyle(fontSize: 26),
+              )),
+              Obx(() => Text(
+                direction_z.value,
+                style: TextStyle(fontSize: 26),
+              )),
+            ]),
+        Obx(
+              () => Text("x: ${dir_x.value}", style: TextStyle(fontSize: 14)),
+        ),
+        Obx(
+              () => Text("y: ${dir_y.value}", style: TextStyle(fontSize: 14)),
+        ),
+        Obx(
+              () => Text("z: ${dir_z.value}", style: TextStyle(fontSize: 14)),
+        ),
+      ],
+    );
+  }
+}
