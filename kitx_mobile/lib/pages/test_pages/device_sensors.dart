21f4eaf6b4aa79fc4bd586922dc01f64a4920290
Dynesshely <catrol@qq.com>
2023-01-30 16:11:06 +0800

    💾 Feat: 加速度数据模块拆分

--- kitx_mobile/lib/pages/test_pages/device_sensors.dart
+++ kitx_mobile/lib/pages/test_pages/device_sensors.dart
@@ -1,8 +1,7 @@
 ﻿import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:kitx_mobile/pages/test_pages/sensors_display_stands/gyroscope_display_stand.dart';
-import 'package:sensors_plus/sensors_plus.dart';
 
+import 'sensors_display_stands/gyroscope_display_stand.dart';
+import 'sensors_display_stands/acceleration_display_stand.dart';
 import 'sensors_display_stands/vibration_display_stand.dart';
 
 class DeviceSensorsPage extends StatefulWidget {
@@ -11,18 +10,9 @@ class DeviceSensorsPage extends StatefulWidget {
 }
 
 class _DeviceSensorsPage extends State<DeviceSensorsPage> {
-  final acc_x = 0.0.obs, acc_y = 0.0.obs, acc_z = 0.0.obs;
 
   @override
   void initState() {
-    userAccelerometerEvents.listen((event) {
-      print(event);
-
-      acc_x.value = event.x;
-      acc_y.value = event.y;
-      acc_z.value = event.z;
-    });
-
     super.initState();
   }
 
@@ -34,16 +24,7 @@ class _DeviceSensorsPage extends State<DeviceSensorsPage> {
           padding: EdgeInsets.all(30),
           child: ListView(children: [
             GyroscopeDisplayStand(),
-            Text("Acceleration Data", style: TextStyle(fontSize: 32)),
-            Obx(
-              () => Text("x: ${acc_x.value}", style: TextStyle(fontSize: 14)),
-            ),
-            Obx(
-              () => Text("y: ${acc_y.value}", style: TextStyle(fontSize: 14)),
-            ),
-            Obx(
-              () => Text("z: ${acc_z.value}", style: TextStyle(fontSize: 14)),
-            ),
+            AccelerationDisplayStand(),
             VibrationDisplayStand(),
           ])),
     );
