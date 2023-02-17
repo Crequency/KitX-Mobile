de435d5e5d3bc418ae21311c5181bab03617eb77
Dynesshely <catrol@qq.com>
2023-02-18 00:10:49 +0800

    💾 Feat(AboutPage): 新增`第三方许可证`, `关于我们`部分

---
+++ kitx_mobile/lib/data/third_party_licenses_provider.dart
@@ -0,0 +1,87 @@
+﻿import 'package:kitx_mobile/models/third_party_data.dart';
+
+var thirdPartyDataList = [
+  new ThirdPartyData("flutter", "v3.7.3", "google", "https://flutter.dev/",
+      "https://github.com/flutter/flutter"),
+  new ThirdPartyData("get", "v4.6.5", "jonataslaw",
+      "https://pub.dev/packages/get", "https://github.com/jonataslaw/getx"),
+  new ThirdPartyData(
+      "network_info_plus",
+      "v2.1.4",
+      "fluttercommunity",
+      "https://pub.dev/packages/network_info_plus",
+      "https://github.com/fluttercommunity/plus_plugins/tree/main/packages/network_info_plus"),
+  new ThirdPartyData(
+      "device_info_plus",
+      "v8.0.0",
+      "fluttercommunity",
+      "https://pub.dev/packages/device_info_plus",
+      "https://github.com/fluttercommunity/plus_plugins/tree/main/packages/device_info_plus"),
+  new ThirdPartyData(
+      "flutter_logs",
+      "v2.1.7",
+      "umair13adil",
+      "https://pub.dev/packages/flutter_logs",
+      "https://github.com/umair13adil/flutter_logs"),
+  new ThirdPartyData(
+      "permission_handler",
+      "v8.1.4",
+      "Baseflow",
+      "https://pub.dev/packages/permission_handler",
+      "https://github.com/baseflow/flutter-permission-handler"),
+  new ThirdPartyData(
+      "mac_address",
+      "v1.0.0",
+      "baabale",
+      "https://pub.dev/packages/mac_address",
+      "https://github.com/baabale/mac_address"),
+  new ThirdPartyData(
+      "community_material_icon",
+      "v5.9.55",
+      "ripsware",
+      "https://pub.dev/packages/community_material_icon",
+      "https://github.com/ripsware/community_material_icon"),
+  new ThirdPartyData(
+      "flutter_blue_plus",
+      "v1.4.0",
+      "boskokg",
+      "https://pub.dev/packages/flutter_blue_plus",
+      "https://github.com/boskokg/flutter_blue_plus"),
+  new ThirdPartyData(
+      "sensors_plus",
+      "v2.0.1",
+      "fluttercommunity",
+      "https://pub.dev/packages/sensors_plus",
+      "https://github.com/fluttercommunity/plus_plugins/tree/main/packages/sensors_plus"),
+  new ThirdPartyData(
+      "vibration",
+      "v1.7.6",
+      "benjamindean",
+      "https://pub.dev/packages/vibration",
+      "https://github.com/benjamindean/flutter_vibration"),
+  new ThirdPartyData(
+      "built_value",
+      "v8.4.3",
+      "google",
+      "https://pub.dev/packages/built_value",
+      "https://github.com/google/built_value.dart/tree/master/built_value"),
+  new ThirdPartyData(
+      "build_collection",
+      "v5.1.1",
+      "google",
+      "https://pub.dev/packages/built_collection",
+      "https://github.com/google/built_collection.dart"),
+  new ThirdPartyData(
+      "package_info_plus",
+      "v3.0.3",
+      "fluttercommunity",
+      "https://pub.dev/packages/package_info_plus",
+      "https://github.com/fluttercommunity/plus_plugins/tree/main/packages/package_info_plus"),
+  new ThirdPartyData(
+      "url_launcher",
+      "v6.1.9",
+      "flutter",
+      "https://pub.dev/packages/url_launcher",
+      "https://github.com/flutter/plugins/tree/main/packages/url_launcher/url_launcher"),
+  // new ThirdPartyData("", "", "", "", ""),
+];
--- kitx_mobile/lib/pages/about_page.dart
+++ kitx_mobile/lib/pages/about_page.dart
@@ -1,8 +1,11 @@
+import 'package:community_material_icon/community_material_icon.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+import '../data/third_party_licenses_provider.dart';
 
 class AboutPage extends StatefulWidget {
   const AboutPage({Key? key}) : super(key: key);
@@ -16,22 +19,30 @@ class _AboutPageState extends State<AboutPage> {
   final iconEntering = false.obs;
   final iconEntered = false.obs;
   final titleEntered = false.obs;
+
   bool contentEntering = false;
+
+  final thirdPartyDataDisplayCount = 1.obs;
 
   @override
   void initState() {
     var packageInfo = PackageInfo.fromPlatform();
     packageInfo.then((value) => version.value = value.version);
+
     Future.delayed(Duration(milliseconds: 150))
         .then((value) => iconEntering.value = true);
+
     Future.delayed(Duration(milliseconds: 400))
         .then((value) => iconEntered.value = true);
+
     Future.delayed(Duration(milliseconds: 600))
         .then((value) => titleEntered.value = true);
+
     Future.delayed(Duration(milliseconds: 0))
         .then((value) => super.setState(() {
               contentEntering = true;
             }));
+
     super.initState();
   }
 
@@ -64,14 +75,18 @@ class _AboutPageState extends State<AboutPage> {
                       // onEnd: () => super.setState(() {
                       //       entered = true;
                       //     }),
-                      child: InkWell(
-                          splashColor: context.iconColor?.withOpacity(0.3),
-                          child: const Image(
-                            alignment: Alignment.center,
-                            image: AssetImage(
-                                "assets/KitX-Icon-1920x-margin-2x.png"),
-                            fit: BoxFit.cover,
-                          )))),
+                      child: AnimatedOpacity(
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.easeInCirc,
+                          opacity: iconEntering.value ? 1 : 0,
+                          child: InkWell(
+                              splashColor: context.iconColor?.withOpacity(0.3),
+                              child: const Image(
+                                alignment: Alignment.center,
+                                image: AssetImage(
+                                    "assets/KitX-Icon-1920x-margin-2x.png"),
+                                fit: BoxFit.cover,
+                              ))))),
                   Container(
                     alignment: Alignment.center,
                     child: Column(
@@ -120,10 +135,12 @@ class _AboutPageState extends State<AboutPage> {
                     Divider(),
                     Container(
                         height: 40,
+                        padding: EdgeInsets.symmetric(horizontal: 15),
                         alignment: Alignment.center,
                         child: ListView(
                             shrinkWrap: true,
                             scrollDirection: Axis.horizontal,
+                            physics: BouncingScrollPhysics(),
                             children: [
                               Chip(label: const Text("Dynesshely")),
                               Container(width: 10),
@@ -148,6 +165,7 @@ class _AboutPageState extends State<AboutPage> {
                         child: ListView(
                             shrinkWrap: true,
                             scrollDirection: Axis.horizontal,
+                            physics: BouncingScrollPhysics(),
                             padding: EdgeInsets.all(10),
                             children: [
                               ElevatedButton(
@@ -160,6 +178,55 @@ class _AboutPageState extends State<AboutPage> {
                                       "https://gitee.com/Crequency/KitX")),
                                   child: Text("Gitee")),
                             ])),
+                    Divider(),
+                    AnimatedContainer(
+                      duration: Duration(milliseconds: 1000),
+                      curve: Curves.easeInOutCubicEmphasized,
+                      height: contentEntering ? 20 : 2400,
+                    ),
+                    Container(
+                        alignment: Alignment.center,
+                        child: Text("AboutPage_ThirdPartyLicenses".tr,
+                            style: TextStyle(fontSize: 20))),
+                    Divider(),
+                    Container(
+                      padding: EdgeInsets.all(15),
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: thirdPartyDataDisplayCount.value,
+                        itemBuilder: thirdPartyLicenseBuilder,
+                      ),
+                      // child: Text("AboutPage_JoinUS_Text".tr),
+                    ),
+                    AnimatedContainer(
+                        height: thirdPartyDataDisplayCount.value !=
+                                thirdPartyDataList.length
+                            ? 55
+                            : 0,
+                        duration: Duration(milliseconds: 1000),
+                        padding: EdgeInsets.fromLTRB(20, 0, 20, 15),
+                        child: ElevatedButton(
+                            onPressed: () => super.setState(() {
+                                  thirdPartyDataDisplayCount.value =
+                                      thirdPartyDataList.length;
+                                }),
+                            child: Text(
+                                "AboutPage_ThirdPartyLicenses_DisplayAll".tr))),
+                    Divider(),
+                    AnimatedContainer(
+                      duration: Duration(milliseconds: 1000),
+                      curve: Curves.easeInOutCubicEmphasized,
+                      height: contentEntering ? 20 : 3200,
+                    ),
+                    Container(
+                        alignment: Alignment.center,
+                        child: Text("AboutPage_JoinUS".tr,
+                            style: TextStyle(fontSize: 20))),
+                    Divider(),
+                    Container(
+                        padding: EdgeInsets.all(20),
+                        child: Text("AboutPage_JoinUS_Text".tr)),
                     Divider(),
                     Container(height: 500),
                   ],
@@ -270,5 +337,58 @@ class _AboutPageState extends State<AboutPage> {
     //         Container(height: 300),
     //       ],
     //     )));
+  }
+
+  Widget thirdPartyLicenseBuilder(BuildContext context, int index) {
+    var thirdPartyData = thirdPartyDataList[index];
+    var url = thirdPartyData.ThirdPartyUrl;
+    var repo = thirdPartyData.ThirdPartyRepo;
+    return Card(
+        child: SizedBox(
+            width: 300,
+            height: 50,
+            child: InkWell(
+              splashColor: Colors.blue.withAlpha(30),
+              onTap: () => print("${thirdPartyData.ThirdPartyName} tapped."),
+              child: ListView(
+                  shrinkWrap: true,
+                  padding: EdgeInsets.all(10),
+                  scrollDirection: Axis.horizontal,
+                  physics: BouncingScrollPhysics(),
+                  children: [
+                    IconButton(
+                      alignment: Alignment.center,
+                      splashRadius: 20,
+                      padding: const EdgeInsets.all(0),
+                      iconSize: 24,
+                      icon: url?.contains("pub.dev") ?? false
+                          ? const Icon(CommunityMaterialIcons.link)
+                          : const Icon(CommunityMaterialIcons.link),
+                      onPressed: () => launchUrl(
+                          Uri.parse(thirdPartyData.ThirdPartyUrl ?? "")),
+                    ),
+                    IconButton(
+                      alignment: Alignment.center,
+                      splashRadius: 20,
+                      padding: const EdgeInsets.all(0),
+                      iconSize: 24,
+                      icon: repo?.contains("github.com") ?? false
+                          ? const Icon(CommunityMaterialIcons.github)
+                          : const Icon(CommunityMaterialIcons.link),
+                      onPressed: () => launchUrl(
+                          Uri.parse(thirdPartyData.ThirdPartyRepo ?? "")),
+                    ),
+                    Container(
+                        alignment: Alignment.center,
+                        child: Text(
+                          thirdPartyData.ThirdPartyName ?? "null",
+                          style: TextStyle(fontSize: 20),
+                        )),
+                    Text(
+                      thirdPartyData.ThirdPartyVersion ?? "null",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ]),
+            )));
   }
 }
