8b3d520611040fd4dfc3b2206f1ef4440e8bbe94
Dynesshely <catrol@qq.com>
2023-02-24 19:16:56 +0800

    💾 Feat: Better HomePage navigation. 1. Added more navigation button on home page. 2. Show devices count founded on home page, more translation items.

--- kitx_mobile/lib/pages/home_page.dart
+++ kitx_mobile/lib/pages/home_page.dart
@@ -7,6 +7,8 @@ import 'test_page.dart';
 import 'settings_page.dart';
 import 'about_page.dart';
 
+import '../utils/global.dart' as global;
+
 import "controls/devices_status.dart";
 
 class HomePage extends StatefulWidget {
@@ -21,85 +23,119 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     // imageCache.clear(); // 清除图片缓存
     return Scaffold(
-      drawerEnableOpenDragGesture: true,
-      drawerEdgeDragWidth: 100,
-      appBar: AppBar(
-        title: Text("IndexPage_Title".tr),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                // color: Colors.blue,
-                image: const DecorationImage(
-                  alignment: Alignment.topCenter,
-                  image: AssetImage("assets/KitX-Background.png"),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              child: Text(
-                "",
-                // "Drawer_Title".tr,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.devices),
-              title: Text("Drawer_Devices".tr),
-              onTap: () {
-                Get.back();
-                Get.to(() => DevicePage());
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.alternate_email),
-              title: Text("Drawer_Account".tr),
-              onTap: () {
-                Get.back();
-                Get.to(() => AccountPage());
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.bug_report),
-              title: Text("Drawer_Test".tr),
-              onTap: () {
-                Get.back();
-                Get.to(() => TestPage());
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text("Drawer_Setting".tr),
-              onTap: () {
-                Get.back();
-                Get.to(() => SettingsPage());
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.info_outline_rounded),
-              title: Text("Drawer_About".tr),
-              onTap: () {
-                Get.back();
-                Get.to(() => AboutPage());
-              },
-            ),
-          ],
+        drawerEnableOpenDragGesture: true,
+        drawerEdgeDragWidth: MediaQuery.of(context).size.width / 2 + 50,
+        appBar: AppBar(
+          title: Text("IndexPage_Title".tr),
         ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            DevicesStatus(),
-            Text(
-              "欢迎来到 KitX",
-              style: TextStyle(
-                fontSize: 20,
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  // color: Colors.blue,
+                  image: const DecorationImage(
+                    alignment: Alignment.topCenter,
+                    image: AssetImage("assets/KitX-Background.png"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Text(
+                  "",
+                  // "Drawer_Title".tr,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.devices),
+                title: Text("Drawer_Devices".tr),
+                onTap: () {
+                  Get.back();
+                  Get.to(() => DevicePage());
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.alternate_email),
+                title: Text("Drawer_Account".tr),
+                onTap: () {
+                  Get.back();
+                  Get.to(() => AccountPage());
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.bug_report),
+                title: Text("Drawer_Test".tr),
+                onTap: () {
+                  Get.back();
+                  Get.to(() => TestPage());
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text("Drawer_Setting".tr),
+                onTap: () {
+                  Get.back();
+                  Get.to(() => SettingsPage());
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.info_outline_rounded),
+                title: Text("Drawer_About".tr),
+                onTap: () {
+                  Get.back();
+                  Get.to(() => AboutPage());
+                },
+              ),
+            ],
+          ),
+        ),
+        body: ListView(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              child: ListView(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                children: [
+                  Obx(
+                    () => ListTile(
+                      leading: Icon(Icons.devices),
+                      title: Text("Drawer_Devices".tr),
+                      subtitle: Text("${global.devices.length.obs} " + "HomePage_DevicesCount".tr),
+                      trailing: Icon(Icons.keyboard_arrow_right),
+                      onTap: () => Get.to(() => DevicePage()),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.alternate_email),
+                    title: Text("Drawer_Account".tr),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                    onTap: () => Get.to(() => AccountPage()),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.bug_report),
+                    title: Text("Drawer_Test".tr),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                    onTap: () => Get.to(() => TestPage()),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text("Drawer_Setting".tr),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                    onTap: () => Get.to(() => SettingsPage()),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.info_outline_rounded),
+                    title: Text("Drawer_About".tr),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                    onTap: () => Get.to(() => AboutPage()),
+                  ),
+                ],
               ),
             ),
           ],
