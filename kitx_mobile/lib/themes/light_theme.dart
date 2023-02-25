32cd8312e765d7cb2c07ece2ee01b7f48d8d15e9
Dynesshely <catrol@qq.com>
2023-02-25 12:15:01 +0800

    💾　Feat: Upgraded to Material 3

--- kitx_mobile/lib/themes/light_theme.dart
+++ kitx_mobile/lib/themes/light_theme.dart
@@ -1,6 +1,7 @@
 ﻿import 'package:flutter/material.dart';
 
 ThemeData GetLightThemeData() => ThemeData(
+      useMaterial3: true,
       primarySwatch: Colors.blue,
       brightness: Brightness.light,
     );
