66fd460b93f3fde20b80c4431cf2bb8db1e4a1af
Dynesshely <catrol@qq.com>
2023-02-26 22:10:31 +0800

    💾 📝 Feat, Chore: Updated dependencies and licenses.

--- kitx_mobile/lib/data/third_party_licenses_provider.dart
+++ kitx_mobile/lib/data/third_party_licenses_provider.dart
@@ -1,12 +1,12 @@
 ﻿import 'package:kitx_mobile/models/third_party_data.dart';
 
 var thirdPartyDataList = [
-  ThirdPartyData("flutter", "v3.7.3", "google", "https://flutter.dev/", "https://github.com/flutter/flutter"),
+  ThirdPartyData("flutter", "v3.7.5", "google", "https://flutter.dev/", "https://github.com/flutter/flutter"),
   ThirdPartyData(
       "get", "v4.6.5", "jonataslaw", "https://pub.dev/packages/get", "https://github.com/jonataslaw/getx"),
   ThirdPartyData(
       "network_info_plus",
-      "v2.1.4",
+      "v3.0.2",
       "fluttercommunity",
       "https://pub.dev/packages/network_info_plus",
       "https://github.com/fluttercommunity/plus_plugins/tree/main/packages/network_info_plus"),
@@ -16,9 +16,9 @@ var thirdPartyDataList = [
       "fluttercommunity",
       "https://pub.dev/packages/device_info_plus",
       "https://github.com/fluttercommunity/plus_plugins/tree/main/packages/device_info_plus"),
-  ThirdPartyData("flutter_logs", "v2.1.7", "umair13adil", "https://pub.dev/packages/flutter_logs",
-      "https://github.com/umair13adil/flutter_logs"),
-  ThirdPartyData("permission_handler", "v8.1.4", "Baseflow", "https://pub.dev/packages/permission_handler",
+  // ThirdPartyData("flutter_logs", "v2.1.7", "umair13adil", "https://pub.dev/packages/flutter_logs",
+  //     "https://github.com/umair13adil/flutter_logs"),
+  ThirdPartyData("permission_handler", "v10.2.0", "Baseflow", "https://pub.dev/packages/permission_handler",
       "https://github.com/baseflow/flutter-permission-handler"),
   ThirdPartyData("mac_address", "v1.0.0", "baabale", "https://pub.dev/packages/mac_address",
       "https://github.com/baabale/mac_address"),
@@ -44,7 +44,9 @@ var thirdPartyDataList = [
       "fluttercommunity",
       "https://pub.dev/packages/package_info_plus",
       "https://github.com/fluttercommunity/plus_plugins/tree/main/packages/package_info_plus"),
-  ThirdPartyData("url_launcher", "v6.1.9", "flutter", "https://pub.dev/packages/url_launcher",
+  ThirdPartyData("url_launcher", "v6.1.10", "flutter", "https://pub.dev/packages/url_launcher",
       "https://github.com/flutter/plugins/tree/main/packages/url_launcher/url_launcher"),
+  ThirdPartyData("f_logs", "v2.0.1", "zubairehman", "https://pub.dev/packages/f_logs",
+      "https://github.com/zubairehman/Flogs"),
   // ThirdPartyData("", "", "", "", ""),
 ];
