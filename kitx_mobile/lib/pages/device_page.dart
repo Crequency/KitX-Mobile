99d239b119a853f8ffa6c3472e1f03a809534b24
Dynesshely <catrol@qq.com>
2023-03-28 01:03:40 +0800

    💾 Feat(DevicePage, DeviceCard): 卡片生成时使用缩放动画进入.

--- kitx_mobile/lib/pages/device_page.dart
+++ kitx_mobile/lib/pages/device_page.dart
@@ -2,14 +2,12 @@ import 'package:community_material_icon/community_material_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'package:kitx_mobile/converters/os_type_2_icon.dart';
-import 'package:kitx_mobile/models/device_info.dart';
-import 'package:kitx_mobile/utils/datetime_format.dart' show datetimeToShortString;
+import 'package:kitx_mobile/pages/controls/device_card.dart';
 import 'package:kitx_mobile/utils/global.dart';
 
 /// Device Page
 class DevicePage extends StatefulWidget {
-  // ignore: public_member_api_docs
+  /// Constructor
   const DevicePage({Key? key}) : super(key: key);
 
   @override
@@ -21,6 +19,7 @@ class _DevicePage extends State<DevicePage> {
 
   var deviceCardHorizontalScale = 0.5;
   var back2topButtonVisibility = false.obs;
+  var justEnteredPage = true;
 
   @override
   void initState() {
@@ -31,6 +30,9 @@ class _DevicePage extends State<DevicePage> {
         back2topButtonVisibility.value = false;
       }
     });
+
+    // Execute after all widgets built.
+    WidgetsBinding.instance.addPostFrameCallback((_) => justEnteredPage = false);
 
     super.initState();
   }
@@ -123,7 +125,12 @@ class _DevicePage extends State<DevicePage> {
                       itemBuilder: (_, index) {
                         var list = Global.deviceService.deviceInfoList;
                         var info = index >= Global.deviceService.length ? null : list[index];
-                        return createDeviceCard(context, info, index);
+                        return DeviceCard(
+                          info,
+                          index,
+                          key: Key('${info?.deviceName ?? ''}${info?.iPv4 ?? ''}'),
+                          shouldDelay: justEnteredPage,
+                        );
                       },
                       onReorder: (int oldIndex, int newIndex) {
                         var list = Global.deviceService.deviceInfoList;
@@ -140,11 +147,13 @@ class _DevicePage extends State<DevicePage> {
                         runSpacing: 0.0,
                         children: [
                           for (var i = 0; i < Global.deviceService.length; ++i)
-                            createDeviceCard(
-                              context,
+                            DeviceCard(
                               Global.deviceService.deviceInfoList[i],
                               i,
                               width: (MediaQuery.of(context).size.width - 20) * deviceCardHorizontalScale,
+                              key: Key('${Global.deviceService.deviceInfoList[i].deviceName}'
+                                  '${Global.deviceService.deviceInfoList[i].iPv4}'),
+                              shouldDelay: justEnteredPage,
                             )
                         ],
                       ),
@@ -152,112 +161,6 @@ class _DevicePage extends State<DevicePage> {
                   ),
           ),
         ],
-      ),
-    );
-  }
-
-  /// Get device display name
-  String getDeviceDisplayName(DeviceInfoStruct info) {
-    var result = info.deviceName;
-
-    if (info.deviceName == Global.deviceName) result += ' ${'DevicePage_LocalDevice'.tr}';
-    if (info.isMainDevice) result += ' ${'DevicePage_MainDevice'.tr}';
-
-    return result;
-  }
-
-  /// Get device card color
-  Color getDeviceCardColor(BuildContext context, DeviceInfoStruct info) {
-    var result = context.theme.cardColor;
-
-    if (info.deviceName == Global.deviceName) result = context.isDarkMode ? Colors.indigo : Colors.limeAccent;
-    if (info.isMainDevice) result = context.isDarkMode ? Colors.deepPurple : Colors.tealAccent;
-
-    return result;
-  }
-
-  var mainDeviceUpdatedSignal = false.obs;
-
-  Widget createDeviceCard(BuildContext context, DeviceInfoStruct? info, int index, {double? width}) {
-    if (info == null) return SizedBox(height: 300, width: width, key: Key('spacer'));
-
-    var _iconStyle = Convert(info.deviceOSType);
-    var _icon = Icon(_iconStyle, size: 36);
-
-    var cardColor = getDeviceCardColor(context, info);
-    var deviceName = getDeviceDisplayName(info);
-
-    return SizedBox(
-      key: Key('${info.deviceName}${info.iPv4}'),
-      width: width,
-      child: FractionallySizedBox(
-        widthFactor: width == null ? 0.9 : 0.98,
-        child: Padding(
-          padding: EdgeInsets.only(bottom: width == null ? 20 : 8),
-          child: Card(
-            color: cardColor,
-            clipBehavior: Clip.hardEdge,
-            child: InkWell(
-              splashColor: context.iconColor?.withOpacity(0.3),
-              onTap: () {},
-              child: Padding(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(5, 2, 0, 2),
-                          child: _icon,
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding: EdgeInsets.fromLTRB(10, 3, 0, 3),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                SingleChildScrollView(
-                                  scrollDirection: Axis.horizontal,
-                                  child: Text(deviceName, style: const TextStyle(fontSize: 18)),
-                                ),
-                                SingleChildScrollView(
-                                  scrollDirection: Axis.horizontal,
-                                  child: Text(info.deviceOSVersion),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Text(datetimeToShortString(info.sendTime), style: const TextStyle(fontSize: 14)),
-                    Text(
-                      'DevicePage_PluginsCountText'.tr + info.pluginsCount.toString(),
-                      style: const TextStyle(fontSize: 14),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('${info.iPv4}:${info.pluginServerPort}', style: const TextStyle(fontSize: 10)),
-                        Flexible(
-                          child: SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
-                            child: Text(
-                              info.iPv6,
-                              textAlign: TextAlign.right,
-                              style: const TextStyle(fontSize: 10),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
       ),
     );
   }
