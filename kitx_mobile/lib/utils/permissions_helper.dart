57477ab67d5c1e332202e6dbb3c73aaa38f8f84c
Dynesshely <catrol@qq.com>
2023-04-17 14:51:01 +0800

    💾 Feat: Request network related permissions when starting.

--- kitx_mobile/lib/utils/permissions_helper.dart
+++ kitx_mobile/lib/utils/permissions_helper.dart
@@ -1,25 +1,34 @@
-﻿import 'package:kitx_mobile/utils/log.dart';
-import 'package:permission_handler/permission_handler.dart';
+﻿import 'package:permission_handler/permission_handler.dart';
 
-/// Request Location Permissions
-Future<bool> requestLocationPermissions() async {
-  Log.info('Checking Location permissions');
-
+/// Request Permission
+Future<bool> requestPermission(Permission target) async {
   var status = await Permission.location.status;
-
-  // Blocked?
   if (status.isDenied || status.isRestricted) {
-    // Ask the user to unblock
-    if (await Permission.location.request().isGranted) {
-      // Either the permission was already granted before or the user just granted it.
-      Log.info('Location permissions granted');
-      return true;
-    } else {
-      Log.info('Location permissions not granted');
-      return false;
-    }
+    return target.request().isGranted;
   } else {
-    Log.info('Permission already granted (previous execution?)');
     return true;
+  }
+}
+
+/// Request Permissions
+Future<List<bool>> requestPermissions(List<Permission> targets) async {
+  var rst = <bool>[];
+  for (var element in targets) {
+    rst.add(await requestPermission(element));
+  }
+  return rst;
+}
+
+/// Request Network Related Permissions
+Future<bool> requestNetworkRelatedPermissions() async {
+  var result = await requestPermissions([
+    Permission.location,
+    Permission.bluetooth,
+  ]);
+
+  if (result.every((element) => element)) {
+    return true;
+  } else {
+    return false;
   }
 }
