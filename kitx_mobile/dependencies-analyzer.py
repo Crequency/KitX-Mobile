28ca2547ce419e70eff475e296a287521683751d
Dynesshely <catrol@qq.com>
2023-07-09 23:36:19 +0800

    📝 Chore(Tools): Use python script to automatically generate third-party data.

--- kitx_mobile/dependencies-analyzer.py
+++ kitx_mobile/dependencies-analyzer.py
@@ -8,6 +8,24 @@ export_for_mobile_about = False
 export_for_website_docs = False
 file_pubspec = "./pubspec.yaml"
 
+mobile_about_format = """  ThirdPartyData(
+    '{}',
+    '{}',
+    '{}',
+    '{}',
+    '{}',
+  ),"""
+website_docs_format = "        <L n=\"{}\" v=\"{}\" u=\"{}\" />"
+
+mobile_about_tags = ["// Begin ThirdPartyDataList", "// End ThirdPartyDataList"]
+website_docs_tags = ["<!--Begin PackagesTable-->", "<!--End PackagesTable-->"]
+
+mobile_about_path = ["./lib/data/third_party_licenses_provider.dart"]
+website_docs_path = [
+    "../../KitX Docs/docs/mobile/guide/packages.md",
+    "../../KitX Docs/docs/en/mobile/guide/packages.md",
+]
+
 for arg in sys.argv:
     if arg == "--export-for-mobile-about":
         export_for_mobile_about = True
@@ -16,25 +34,90 @@ for arg in sys.argv:
         export_for_website_docs = True
         export_for_mobile_about = False
 
-with open (file_pubspec, "r") as stream:
+with open(file_pubspec, "r") as stream:
     data = yaml.load(stream, Loader=yaml.FullLoader)
     dependencies = data["dependencies"]
+
+    result = []
+
     for dependency in dependencies:
         name = dependency
-        version = dependencies[name]
+        version = ""
+
+        if type(dependencies[name]) is str:
+            version = dependencies[name]
+        else:
+            version = ""
+            if name == "flutter":
+                version = "^" + re.search(">=(\S*) <", data["environment"]["sdk"]).group(1)
+
+        print("({}/{}) Fetching `{}` ...".format(len(result), len(dependencies.keys()), name))
+
         url_pub = "https://pub.dev/packages/" + name
         url_pub_api = "https://pub.dev/api/packages/" + name
         url_github = ""
+
         author_name = ""
         description = ""
+
         pub_result = requests.get(url_pub_api)
         pub_result_analyzed = json.loads(pub_result.text)
-        if "repository" in pub_result_analyzed["latest"]["pubspec"].keys():
-            url_github = pub_result_analyzed["latest"]["pubspec"]["repository"]
-            author_name = re.search("github.com/(\S*)/", url_github).group(1).split('/')[0]
-        if "description" in pub_result_analyzed["latest"]["pubspec"].keys():
-            description = pub_result_analyzed["latest"]["pubspec"]["description"]
+
+        pubspec = pub_result_analyzed["latest"]["pubspec"]
+
+        if "repository" in pubspec.keys():
+            url_github = pubspec["repository"]
+            if url_github.find("github"):
+                author_name = re.search("github.com/(\S*)/", url_github).group(1).split('/')[0]
+
+        if "description" in pubspec.keys():
+            description = pubspec["description"]
+
         if export_for_mobile_about:
-            print("ThirdPartyData('{}','{}','{}','{}','{}',),".format(name, version, author_name, url_pub, url_github))
+            result.append(mobile_about_format.format(name, version, author_name, url_pub, url_github))
         if export_for_website_docs:
-            print("<L n=\"{}\" v=\"{}\" u=\"{}\" />".format(name, version, description))
+            result.append(website_docs_format.format(name, version, description))
+
+    print("({}/{}) All fetched !".format(len(result), len(dependencies.keys())))
+
+    writtenPath = []
+    writtenTags = []
+
+    if export_for_mobile_about:
+        writtenPath = mobile_about_path
+        writtenTags = mobile_about_tags
+    if export_for_website_docs:
+        writtenPath = website_docs_path
+        writtenTags = website_docs_tags
+
+    for file in writtenPath:
+        with open(file, "r+") as stream:
+            lines = stream.readlines()
+
+            writtenIn = ""
+
+            beginIndex = -1
+            endIndex = -1
+            index = 0
+
+            for line in lines:
+                if line.find(writtenTags[0]) != -1:
+                    beginIndex = index
+                if line.find(writtenTags[1]) != -1:
+                    endIndex = index
+                index += 1
+
+            for i in range(0, beginIndex + 1):
+                writtenIn += lines[i]
+            for line in result:
+                writtenIn += line + "\n"
+            for i in range(endIndex, len(lines)):
+                writtenIn += lines[i]
+
+            print(writtenIn)
+
+            if input("Do you want to write this in `{}` ? (y/n) ".format(file)).lower() != "n":
+                stream.seek(0)
+                stream.write(writtenIn)
+                stream.truncate()
+                print("Written in `{}` !".format(file))
